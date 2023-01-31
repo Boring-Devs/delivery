3ce7186e5c6c4bc7a54915f1107aee0aed3a0963
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-31 01:18:26 +0100

    feat: add eq operator

--- src/delievery/utils/operators.ts
+++ src/delievery/utils/operators.ts
@@ -1,5 +1,7 @@
 export const curry = (fn: Function, ...args: any[]) =>
   (...newArgs: any[]) => fn(...args, ...newArgs);
 
+export const eq = curry(<T=any>(a: T, b: T) => a === b);
+
 export const runIfExists = 
   curry((fn?: Function, ...args: any[]) => fn ? fn(...args) : fn);
