a7fd74e13cf52396201d4cd6e8008ccb0eed0a93
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-31 01:59:06 +0100

    feat: optimize algo replace foreach to find

--- src/delievery/index.ts
+++ src/delievery/index.ts
@@ -1,8 +1,8 @@
 // Todo: remove node-fetch types to prevent coupling. Build our own types to reduce coupling.
 import fetch, { RequestInfo, RequestInit } from 'node-fetch';
 
-import { mapStatusToDeliveryFunction } from '@/delievery/core/contracts/http';
-import { runIfExists } from '@/delievery/utils/operators';
+import { mapStatusDelivery } from '@/delievery/core/contracts/http';
+import { eq, runIfExists } from '@/delievery/utils/operators';
 import { Devilery } from '@/delievery/data/delievery';
 
 export const delivery = (url: RequestInfo, init?: RequestInit) => {
@@ -12,20 +12,16 @@ export const delivery = (url: RequestInfo, init?: RequestInit) => {
     .then((response) => {
       const { status: statusCode } = response;
       const isOk = statusCode > 199 && statusCode < 300;
-
       if (isOk) runIfExists(deliveryObject.onSuccess, response);
-
-      mapStatusToDeliveryFunction.forEach(deliveryStatus => {
-        if (statusCode === deliveryStatus.status) {
-          const key = deliveryStatus.key as keyof typeof deliveryObject;
-          runIfExists(deliveryObject[key], new Error(deliveryStatus.message));
-        }
-      });
-
-      response.json()
+      const deliveryStatus = mapStatusDelivery.find(eq(statusCode));
+      if (deliveryStatus) {
+        const key = deliveryStatus.key as keyof typeof deliveryObject;
+        runIfExists(deliveryObject[key], new Error(deliveryStatus.message));
+      }
+      response
+        .json()
         .then(data => runIfExists(deliveryObject.onJson, !isOk, data))
-        .catch((_) => runIfExists(
-          deliveryObject.onClientError,
+        .catch((_) => runIfExists(deliveryObject.onClientError,
           new Error("Coultn't parse data to json")
         ));
     })
